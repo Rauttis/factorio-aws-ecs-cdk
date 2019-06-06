7a3b5a5966ad28755b8a63b0c886e1b51cb7fe35
Niklas Appelroth <niklas.appelroth@gmail.com>
2019-06-06 04:19:45 +0300

    Extract FactorioFileSystem and FactorioAutoScalingGroup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import * as autoscaling from '@aws-cdk/aws-autoscaling'
 import * as ec2 from '@aws-cdk/aws-ec2'
 import * as ecs from '@aws-cdk/aws-ecs'
 import * as cdk from '@aws-cdk/cdk'
-import * as efs from '@aws-cdk/aws-efs'
+import { FactorioFileSystem } from './file-system'
+import { FactorioAutoScalingGroup } from './auto-scaling-group'
 
 class FactorioECSCluster extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -10,53 +10,11 @@ class FactorioECSCluster extends cdk.Stack {
 
     const vpc = new ec2.Vpc(this, 'FactorioVpc', { maxAZs: 2 })
 
-    const efsSecurityGroup = new ec2.SecurityGroup(this, 'FactorioEFSSecurityGroup', { vpc })
-    efsSecurityGroup.addIngressRule(efsSecurityGroup, new ec2.TcpPortRange(2049, 2049))
-
-    const fileSystem = new efs.CfnFileSystem(this, 'FactorioFileSystem')
-    const mount0 = new efs.CfnMountTarget(this, 'Subnet0Mount', {
-      fileSystemId: fileSystem.fileSystemId,
-      subnetId: vpc.publicSubnets[0].subnetId,
-      securityGroups: [efsSecurityGroup.securityGroupId],
-    })
-    const mount1 = new efs.CfnMountTarget(this, 'Subnet1Mount', {
-      fileSystemId: fileSystem.fileSystemId,
-      subnetId: vpc.publicSubnets[1].subnetId,
-      securityGroups: [efsSecurityGroup.securityGroupId],
-    })
-
-    const asgSecurityGroup = new ec2.SecurityGroup(this, 'FactorioEC2SecurityGroup', { vpc })
-    asgSecurityGroup.addIngressRule(new ec2.AnyIPv4, new ec2.UdpPortRange(34197, 34197))
-    // TODO: Limit ip
-    asgSecurityGroup.addIngressRule(new ec2.AnyIPv4, new ec2.TcpPort(22))
-    const asg = new autoscaling.AutoScalingGroup(this, 'FactorioAutoScalingGroup', {
-      // TODO: instance type from param
-      instanceType: new ec2.InstanceType('m3.medium'),
-      machineImage: new ecs.EcsOptimizedAmi(),
-      updateType: autoscaling.UpdateType.ReplacingUpdate,
-      minCapacity: 1,
-      maxCapacity: 1,
-      desiredCapacity: 1,
-      vpc,
-      // TODO: spot price from param
-      spotPrice: '0.05',
-      associatePublicIpAddress: true,
-      vpcSubnets: { subnetType: ec2.SubnetType.Public },
-      // TODO: key name from params
-      keyName: 'factorio'
-    })
-    asg.addUserData(`
-      #!/bin/bash -xe
-      yum install -y amazon-efs-utils
-      mkdir /opt/factorio
-      mount -t efs ${fileSystem.ref}:/ /opt/factorio
-      chown 845:845 /opt/factorio
-    `)
-    asg.addSecurityGroup(asgSecurityGroup)
-    asg.node.addDependency(fileSystem, mount0, mount1)
+    const fileSystem = new FactorioFileSystem(this, 'FactorioFileSystem', { vpc })
+    const autoscalingGroup = new FactorioAutoScalingGroup(this, 'FactorioAutoScalingGroup', { vpc, fileSystem })
 
     const cluster = new ecs.Cluster(this, 'FactorioEcsCluster', { vpc })
-    cluster.addAutoScalingGroup(asg)
+    cluster.addAutoScalingGroup(autoscalingGroup)
 
     const taskDefinition = new ecs.Ec2TaskDefinition(this, 'FactorioTaskDefinition')
     const VOLUME_NAME = 'FactorioVolume'
@@ -82,7 +40,7 @@ class FactorioECSCluster extends cdk.Stack {
       {containerPath: '/factorio', sourceVolume: VOLUME_NAME, readOnly: false}
     )
 
-    const service = new ecs.Ec2Service(this, 'FactorioService', {
+    new ecs.Ec2Service(this, 'FactorioService', {
       cluster,
       taskDefinition,
       desiredCount: 1,
